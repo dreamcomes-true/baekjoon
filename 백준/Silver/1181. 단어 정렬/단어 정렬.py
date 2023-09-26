52f929485159181666437afabc166b2ec5f6c313
parkheeddong <116873401+parkheeddong@users.noreply.github.com>
2023-09-26 20:32:58 +0900

    [Silver V] Title: 단어 정렬, Time: 820 ms, Memory: 35868 KB -BaekjoonHub

---
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.py
@@ -0,0 +1,11 @@
+n = int(input())
+words = []
+for _ in range(n) :
+    words.append(input())
+
+words = list(set(words))
+words.sort()
+words.sort(key = lambda x: len(x))
+
+for i in words :
+    print(i)
