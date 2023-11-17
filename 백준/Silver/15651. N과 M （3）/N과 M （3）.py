1c8997b38528813f131f839f022205b60085efa6
parkheeddong <116873401+parkheeddong@users.noreply.github.com>
2023-11-17 14:08:32 +0900

    [Silver III] Title: N과 M (3), Time: 1428 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.py
@@ -0,0 +1,17 @@
+n, m = map(int, input().split())
+
+numlist = []
+
+def backtracking() :
+    if len(numlist) == m :
+        print(" ".join(map(str, numlist)))
+        return;
+
+    for i in range(1, n+1) :    # 1부터 n까지의 숫자에 대해서
+        numlist.append(i)
+        backtracking()
+        numlist.pop()
+
+
+
+backtracking()
